79e66d7be109cb5207eee38943f390772950bebd
Norman Eshuchi <normaneshuchi@gmail.com>
2024-04-25 10:50:59 +0300

    added missing util files

---
+++ src/apps/aviator/store/bets.store.ts
@@ -0,0 +1,39 @@
+import { create } from "zustand";
+import { getTestData } from "../common/getTestData.ts";
+import createStore from "../common/atoms/atoms.ts";
+
+export type TBetSlip = {
+  amount: number;
+  gameId: string;
+  startTime?: number;
+  endTime?: number;
+  exitTime?: number;
+  username?: string;
+  round?: number;
+  index?: number;
+};
+
+interface IBetSlipStore {
+  myBetSlips: TBetSlip[];
+  allBetSlips: TBetSlip[];
+  topBetSlips: TBetSlip[];
+  currentGameID: string;
+  previousGameID: string;
+
+  previousRounds: number[];
+}
+
+const testRoundsData = getTestData();
+
+export const useBetSlipStore = create<IBetSlipStore>()(() => ({
+  myBetSlips: [] as TBetSlip[],
+  allBetSlips: [] as TBetSlip[],
+  topBetSlips: [] as TBetSlip[],
+  currentGameID: "",
+  previousGameID: "",
+  previousRounds: testRoundsData,
+}));
+
+
+
+const betStore = createStore({});
---
+++ src/apps/aviator/styles/breakpoints.ts
@@ -0,0 +1,4 @@
+export const breakpoints = [576, 768, 992, 1200];
+export const MEDIA_QUERIES = breakpoints.map(
+  (bp) => `@media (max-width: ${bp}px)`,
+);
